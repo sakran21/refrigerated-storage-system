23c464fddf99a87427c362faea9c421426dda5dd
Sakran21 <moham.alsakran@gmail.com>
2026-06-09 10:37:01 +0300

    Add backend xUnit test project

---
+++ backend.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void BasicMath_Works()
+    {
+        var result = 2 + 2;
+
+        Assert.Equal(4, result);
+    }
+}
